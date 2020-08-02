b1bed366704a4c8db2cf86578601242e04253c87
Ethan Chau <echau18@cs.uw.edu>
2020-08-01 23:56:10 -0700

    remove bkpt

--- allennlp/models/reading_comprehension/bidaf.py
+++ allennlp/models/reading_comprehension/bidaf.py
@@ -179,7 +179,6 @@ class BidirectionalAttentionFlow(Model):
         question_lstm_mask = question_mask if self._mask_lstms else None
         passage_lstm_mask = passage_mask if self._mask_lstms else None
 
-        import pdb; pdb.set_trace()
         encoded_question = self._dropout(self._phrase_layer(embedded_question, question_lstm_mask))
         encoded_passage = self._dropout(self._phrase_layer(embedded_passage, passage_lstm_mask))
         encoding_dim = encoded_question.size(-1)
