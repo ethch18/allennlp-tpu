015829082cd861ea827288e8fd15fdec1928e8c2
Ethan Chau <echau18@cs.uw.edu>
2020-08-02 20:08:03 -0700

    debug

--- allennlp/models/reading_comprehension/bidaf.py
+++ allennlp/models/reading_comprehension/bidaf.py
@@ -178,6 +178,7 @@ class BidirectionalAttentionFlow(Model):
         passage_mask = util.get_text_field_mask(passage).float()
         question_lstm_mask = question_mask if self._mask_lstms else None
         passage_lstm_mask = passage_mask if self._mask_lstms else None
+        import pdb; pdb.set_trace()
 
         encoded_question = self._dropout(self._phrase_layer(embedded_question, question_lstm_mask))
         encoded_passage = self._dropout(self._phrase_layer(embedded_passage, passage_lstm_mask))
